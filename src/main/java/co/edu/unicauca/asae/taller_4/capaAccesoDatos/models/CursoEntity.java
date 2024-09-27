a4e10a34a19efca3b92848a513119e0570c93e36
ArnoldCatamuscay <ascatamuscay@unicauca.edu.co>
2024-09-27 18:10:12 -0500

    Add attributes, annotations, and repositories for Asignatura, Curso, Espacio Fisico, Franja Horario

--- src/main/java/co/edu/unicauca/asae/taller_4/capaAccesoDatos/models/CursoEntity.java
+++ src/main/java/co/edu/unicauca/asae/taller_4/capaAccesoDatos/models/CursoEntity.java
@@ -1,5 +1,40 @@
 package co.edu.unicauca.asae.taller_4.capaAccesoDatos.models;
 
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "Cursos")
 public class CursoEntity {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer id;
+    @Column(length = 255)
+    String nombre;
+
+    //* Relaciones */
+    @ManyToOne
+    @JoinColumn(name = "idAsignatura", nullable = false)
+	private AsignaturaEntity objAsignatura;
+
+    @OneToMany(fetch = FetchType.EAGER, mappedBy = "objCurso")
+	private List<FranjaHorarioEntity> franjasHorario;
 }
---
+++ src/main/java/co/edu/unicauca/asae/taller_4/capaAccesoDatos/repositories/EspacioFisicoRepository.java
@@ -0,0 +1,9 @@
+package co.edu.unicauca.asae.taller_4.capaAccesoDatos.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import co.edu.unicauca.asae.taller_4.capaAccesoDatos.models.EspacioFisicoEntity;
+
+public interface EspacioFisicoRepository extends CrudRepository<EspacioFisicoEntity, Integer> {
+    
+}
