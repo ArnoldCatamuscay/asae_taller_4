0b8fa5d8e4dbd1dd9b26e1dab5768e0f7a05d16f
Cristian <cristianenv13@gmail.com>
2024-09-27 16:28:15 -0500

    Relacion Curso-Docente

--- src/main/java/co/edu/unicauca/asae/taller_4/capaAccesoDatos/models/DocenteEntity.java
+++ src/main/java/co/edu/unicauca/asae/taller_4/capaAccesoDatos/models/DocenteEntity.java
@@ -1,7 +1,12 @@
 package co.edu.unicauca.asae.taller_4.capaAccesoDatos.models;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.PrimaryKeyJoinColumn;
 import jakarta.persistence.Table;
@@ -20,6 +25,10 @@ public class DocenteEntity extends PersonaEntity {
     @OneToOne
     @JoinColumn(name = "idOficina")
     private OficinaEntity objOficina;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "curso_docente", joinColumns = @JoinColumn(name = "idDocente"), inverseJoinColumns = @JoinColumn(name = "idCurso"))
+    private List<CursoEntity> cursos;
 
     public DocenteEntity(int idPersona, String nombre, String apellido, String correo) {
         super(idPersona, nombre, apellido, correo);
